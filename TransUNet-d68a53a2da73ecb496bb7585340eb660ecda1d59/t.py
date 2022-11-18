55913e670a4487e791fe4cf73d7eb298af721b38
Ma Xihan <xma4@wpi.edu>
2022-11-18 12:37:18 -0500

    delete old data

--- TransUNet-d68a53a2da73ecb496bb7585340eb660ecda1d59/t.py
+++
@@ -1,4 +0,0 @@
-import cv2
-from networks.vit_seg_modeling import VisionTransformer as ViT_seg
-net = ViT_seg(config_vit, img_size=args.img_size, num_classes=config_vit.n_classes).cuda()
-net.load_from(weights=np.load(config_vit.pretrained_path))
