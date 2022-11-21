b6676617db3b5014e8c55d692413b2c64ace29a7
Ma Xihan <xma4@wpi.edu>
2022-11-20 22:21:37 -0500

    add new patient dataset

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -13,7 +13,7 @@ import numpy as np
 from scipy import ndimage
 from torch.utils.data import Dataset
 from PIL import Image
-# from scipy.ndimage.interpolation import zoom
+from scipy.ndimage.interpolation import zoom
 
 
 def random_rot_flip(image, label):
@@ -27,32 +27,37 @@ def random_rot_flip(image, label):
 
 
 def random_rotate(image, label):
-  angle = np.random.randint(-20, 20)
+  angle = np.random.randint(-10, 10)
   image = ndimage.rotate(image, angle, order=0, reshape=False)
   label = ndimage.rotate(label, angle, order=0, reshape=False)
   return image, label
 
 
-# class RandomGenerator(object):
-#   def __init__(self, output_size):
-#     self.output_size = output_size
+def random_deform(image, label):
+  # TODO: elastic deformation
+  return image, label
 
-#   def __call__(self, sample):
-#     image, label = sample['image'], sample['label']
 
-#     if random.random() > 0.5:
-#       image, label = random_rot_flip(image, label)
-#     elif random.random() > 0.5:
-#       image, label = random_rotate(image, label)
-#     x, y = image.shape
-#     if x != self.output_size[0] or y != self.output_size[1]:
-#       image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y), order=3)  # why not 3?
-#       #label = zoom(label, (self.output_size[0] / x, self.output_size[1] / y), order=0)
-#       label = zoom(label, (self.output_size[0] / x, self.output_size[1] / y, 3), order=0)
-#     image = torch.from_numpy(image.astype(np.float32)).unsqueeze(0)
-#     label = torch.from_numpy(label.astype(np.float32))
-#     sample = {'image': image, 'label': label.long()}
-#     return sample
+class RandomGenerator(object):
+  def __init__(self, output_size):
+    self.output_size = output_size
+
+  def __call__(self, sample):
+    image, label = sample['image'], sample['label']
+
+    if random.random() > 0.5:
+      image, label = random_rot_flip(image, label)
+    elif random.random() > 0.5:
+      image, label = random_rotate(image, label)
+    x, y = image.shape
+    if x != self.output_size[0] or y != self.output_size[1]:
+      image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y), order=3)  # why not 3?
+      #label = zoom(label, (self.output_size[0] / x, self.output_size[1] / y), order=0)
+      label = zoom(label, (self.output_size[0] / x, self.output_size[1] / y, 3), order=0)
+    image = torch.from_numpy(image.astype(np.float32)).unsqueeze(0)
+    label = torch.from_numpy(label.astype(np.float32))
+    sample = {'image': image, 'label': label.long()}
+    return sample
 
 
 class LUSDataset(Dataset):
@@ -92,12 +97,6 @@ class LUSDataset(Dataset):
     # print(self.msk_dir+msk_names[idx])
     msk = cv2.imread(self.msk_dir+msk_names[idx], cv2.IMREAD_GRAYSCALE)
     img = cv2.imread(self.img_dir+img_names[idx], cv2.IMREAD_GRAYSCALE)
-    # msk = Image.open(self.img_dir+msk_names[idx]).convert('L')
-    # img = Image.open(self.img_dir+img_names[idx])
-    print("Image size ------------")
-    #print(f"size of img: {img.size}")
-    #print(f"size of mask: {msk.size}")
-
     #img = cv2.imread(self.img_dir+img_names[idx])
     #msk = cv2.imread(self.img_dir+msk_names[idx])
     #print("cv2 size------------")
